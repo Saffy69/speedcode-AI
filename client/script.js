d1b9b1e7a715c8d63f332d64e2a4e49c4cec9613
Saffy <saffy@safal-linux.Safal>
2023-02-03 17:16:43 +0545

    variable change

--- client/script.js
+++ client/script.js
@@ -84,11 +84,10 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://speed-code-ai.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-     
     },
     body: JSON.stringify({
       prompt: data.get("prompt"),
@@ -107,7 +106,6 @@ const handleSubmit = async (e) => {
     const err = await response.text();
 
     messageDiv.innerHTML = "Something went wrong";
-    alert(err);
   }
 };
 
