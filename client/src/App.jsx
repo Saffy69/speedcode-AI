89ab965cf1157182889b63b791d7d6ec28ade81e
Saffy <saffy@safal-linux.Safal>
2023-02-03 19:06:30 +0545

    updated to react

---
+++ client/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import "./App.css";
+import sendImg from "./assets/send.svg";
+import botImg from "./assets/bot.svg";
+import userImg from "./assets/user.svg";
+
+function App() {
+  const [chatLog, setChatLog] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newChatLog = [...chatLog, { user: "me", message: `${input}` }];
+    setChatLog(newChatLog);
+    setInput("");
+    const messages = newChatLog.map((result) => result.message).join("\n");
+    const response = await fetch("http://localhost:5000/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: messages,
+      }),
+    });
+    const data = await response.json();
+    setChatLog([...newChatLog, { user: "gpt", message: data.message.trim() }]);
+  };
+  return (
+    <div id="app">
+      <div id="chat_container">
+        {chatLog.map((result, index) => (
+          <ChatMessage key={index} result={result} />
+        ))}
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <textarea
+          name="prompt"
+          rows="1"
+          cols="1"
+          placeholder="Ask SpeedCode..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></textarea>
+        <button type="submit">
+          <img src={sendImg} alt="send" />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+const ChatMessage = ({ result }) => {
+  return (
+    <div className={`wrapper ${result.user === "gpt" && "ai"}`}>
+      <div className="chat">
+        <div className="profile">
+          <img src={`${result.user === "gpt" ? botImg : userImg}`} />
+        </div>
+        <div className="message">{result.message}</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
